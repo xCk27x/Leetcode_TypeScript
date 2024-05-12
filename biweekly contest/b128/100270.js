fd37757208c7bd4fb0809cf0eee3a665001b813d
xCk27x <pdes0722@gmail.com>
2024-05-13 00:37:00 +0800

    5/13

--- biweekly contest/b128/100270.js
+++
@@ -1,9 +0,0 @@
-function scoreOfString(s) {
-    var sum = 0;
-    for (var i = 1; i < s.length; i++) {
-        sum += Math.abs(s.charCodeAt(i - 1) - s.charCodeAt(i));
-    }
-    return sum;
-}
-;
-console.log(scoreOfString("hello")); // 2
